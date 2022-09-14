c656c4dd7e0e99ac980e9f7e44a2eac9877078a3
Giselle Hoekveld <gisellehoekveld@gmail.com>
2022-09-13 22:49:42 -0300

    create routes

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,17 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+  return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+  const gameId = request.params.id;
+
   return response.json([
     { id: 1, name: 'Ads 1'},
     { id: 2, name: 'Ads 2'},
@@ -10,6 +20,12 @@ app.get('/ads', (request, response) => {
     { id: 4, name: 'Ads 4'},
     { id: 5, name: 'Ads 5'},
   ]);
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+  const adId = request.params.id;
+  
+  return response.json([]);
 });
 
 app.listen(3333);
