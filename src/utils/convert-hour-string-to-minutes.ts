47cab547b8d5862b5f4179739c9516bb4b461dc5
Giselle Hoekveld <gisellehoekveld@gmail.com>
2022-09-14 00:19:33 -0300

    create function convert hour string to minutes

---
+++ src/utils/convert-hour-string-to-minutes.ts
@@ -0,0 +1,7 @@
+export function convertHourStringToMinutes(hoursString: string) {
+  const [hours, minutes] = hoursString.split(':').map(Number);
+
+  const minutesAmount = (hours * 60) + minutes;
+
+  return minutesAmount;
+}
